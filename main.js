2289a630fc85d39ed1477d9acd10f0d4b22f511c
Aleks9080 <65893630+Aleks9080@users.noreply.github.com>
2022-08-10 13:19:30 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+$('.rev_slider').slick({
+    infinite: true,
+    arrows:true,
+    dots:true,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    prevArrow: "<img src='/img/expoparts.ru/arrow-down.svg' class='slick-prev' alt='1'>",
+    nextArrow: "<img src='/img/expoparts.ru/arrow-down-sign.svg' class='slick-next' alt='2'>",
+    responsive: [
+      {
+        breakpoint: 1110,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  $('.slider_marki').slick({
+    infinite: true,
+    arrows:true,
+    dots:true,
+    slidesToShow: 9,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    prevArrow: "<img src='/img/expoparts.ru/arrow-down-sign.svg' class='slick-next pos' alt='1'>",
+    nextArrow: "<img src='/img/expoparts.ru/arrow-down.svg' class='slick-prev pos' alt='2'>",
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
